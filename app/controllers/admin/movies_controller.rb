f12d0aec604f4ebc8f696d84721325c18cd3be60
hojoko <hojoko03@gmail.com>
2022-01-10 17:57:41 +0900

    station3

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,24 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end  
+  
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = "作成しました"
+      redirect_to admin_movies_path
+    else
+      flash[:alert] = "作成できませんでした"
+      render :new
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:name, :image_url, :year, :description, :is_showing)
+    end
 end
