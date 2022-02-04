3958b2876f7ca58608a1ba355cee9e92993db425
hojoko <hojoko03@gmail.com>
2022-02-04 16:08:09 +0900

    station6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
-  def index
-    @movies = Movie.all
+  def index  
+    @movies = Movie.search(search_params[:word], search_params[:is_showing])
   end
+
+  private
+    def search_params
+      params.permit(:word, :is_showing)
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,16 @@ class Movie < ApplicationRecord
   validates :is_showing, presence: true  
   validates :description, presence: true
   validates :image_url, presence: true
+
+  def self.search(word, is_showing)
+    @movies = Movie.all
+    if word.present?
+      @movies = @movies.where('name LIKE ? or description LIKE ?', "%#{word}%", "%#{word}%" )
+    end
+    if is_showing.present?
+      @movies = @movies.where(is_showing: is_showing)
+    else
+      @movies 
+    end
+  end
 end
