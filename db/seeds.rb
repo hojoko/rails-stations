88831f8e98d3b4f6e911695ec8eb51e25a7d5dd2
hojoko <hojoko03@gmail.com>
2022-02-06 12:58:21 +0900

    station7

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,11 @@ Movie.create(name: 'movie1', image_url: 'https://picsum.photos/200', year: 2000,
 Movie.create(name: 'movie2', image_url: 'https://picsum.photos/200', year: 2000, description: '概要です', is_showing: 1)
 Movie.create(name: 'movie3', image_url: 'https://picsum.photos/200', year: 2000, description: '概要です', is_showing: 0)
 Movie.create(name: 'movie4', image_url: 'https://picsum.photos/200', year: 2000, description: '概要です', is_showing: 0)
+
+
+#席のマスターデータ
+(1..5).each do |col|
+  ('a'..'c').each do |row|
+    Sheet.create(column: col, row: row)
+  end
+end
